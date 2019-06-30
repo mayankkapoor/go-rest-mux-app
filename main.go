c563a6cf913e37cee7c1630868d587342d8044d0
Mayank Kapoor <mayankkapoormail@gmail.com>
2019-06-30 09:51:35 +0530

    Update main.go and integerate with MySQL database
    After setting up a MySQL database with a database called "dev", table 
    called "posts" with two columns "id" and "title", the main.go file was 
    updated to talk to the MySQL database running on docker. A 
    mysql-stack.yml is included to setup the database using docker stack 
    (docker stack deploy)

--- main.go
+++ main.go
@@ -1,84 +1,154 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"math/rand"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-	"strconv"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
+
+var db *sql.DB
+var err error
 
 type Post struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
-	Body  string `json:"body"`
+	// Comment out Body for now
+	//Body  string `json:"body"`
 }
 
 var posts []Post
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var posts []Post
+	result, err := db.Query("SELECT id, title FROM posts")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.ID, &post.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
+
 	json.NewEncoder(w).Encode(posts)
 }
 
 func createPost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var post Post
-	_ = json.NewDecoder(r.Body).Decode(&post)
-	post.ID = strconv.Itoa(rand.Intn(1000000))
-	posts = append(posts, post)
-	json.NewEncoder(w).Encode(&post)
+	stmt, err := db.Prepare("INSERT INTO posts(title) VALUES(?)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	title := keyVal["title"]
+
+	_, err = stmt.Exec(title)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "New post was created")
 }
 
 func getPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range posts {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
+
+	result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	var post Post
+	for result.Next() {
+		err := result.Scan(&post.ID, &post.Title)
+		if err != nil {
+			panic(err.Error())
 		}
 	}
-	json.NewEncoder(w).Encode(&Post{})
+
+	json.NewEncoder(w).Encode(post)
 }
 
 func updatePost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range posts {
-		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1:]...)
-			var post Post
-			_ = json.NewDecoder(r.Body).Decode(&post)
-			post.ID = params["id"]
-			posts = append(posts, post)
-			json.NewEncoder(w).Encode(&post)
-			return
-		}
+
+	stmt, err := db.Prepare("UPDATE posts SET title = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
 	}
-	json.NewEncoder(w).Encode(posts)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newTitle := keyVal["title"]
+
+	_, err = stmt.Exec(newTitle, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Post with ID = %s was updated", params["id"])
 }
 
 func deletePost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range posts {
-		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1:]...)
-			break
-		}
+
+	stmt, err := db.Prepare("DELETE FROM posts WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
 	}
-	json.NewEncoder(w).Encode(posts)
+
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
+
 }
 
 func main() {
+	// Establish DB connection
+	db, err = sql.Open("mysql", "mayankkapoor:password@tcp(127.0.0.1:3306)/dev")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	// Initialize data
+	posts = append(posts, Post{ID: "1", Title: "My first post"})
+
+	// Create router and routes
 	router := mux.NewRouter()
-	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
 	router.HandleFunc("/posts", getPosts).Methods("GET")
 	router.HandleFunc("/posts", createPost).Methods("POST")
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
 	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
 	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+
 	http.ListenAndServe(":8000", router)
 }
