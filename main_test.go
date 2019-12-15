8776ad16d88caf4bc6acd1ef9906bbc0da901542
Mayank Kapoor <mayankkapoormail@gmail.com>
2019-12-15 20:48:24 +0530

    Add unit test for getPosts

--- main_test.go
+++ main_test.go
@@ -3,7 +3,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
 )
 
 func TestSum(t *testing.T) {
@@ -11,6 +17,61 @@ func TestSum(t *testing.T) {
 	expected := 9
 	if total != expected {
 		t.Errorf("Sum was incorrect, got: %d, expected: %d.", total, expected)
+	}
+
+}
+
+func (a *api) assertJSON(actual []byte, data interface{}, t *testing.T) {
+	expected, err := json.Marshal(data)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when marshaling expected json data", err)
+	}
+
+	if bytes.Compare(expected, actual) != 0 {
+		t.Errorf("the expected json: %s is different from actual %s", expected, actual)
+	}
+}
+
+func TestGetPostsReturnsPosts(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	// create app with mocked db, request and response to test
+	app := &api{db}
+	req, err := http.NewRequest("GET", "http://localhost/posts", nil)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected while creating request", err)
+	}
+	w := httptest.NewRecorder()
+
+	// before we actually execute our api function, we need to expect required DB actions
+	rows := sqlmock.NewRows([]string{"id", "title"}).
+		AddRow(1, "post one title").
+		AddRow(2, "post two title")
+
+	mock.ExpectQuery("^SELECT (.+) FROM posts$").WillReturnRows(rows)
+
+	// now we execute our request
+	app.getPosts(w, req)
+
+	if w.Code != 200 {
+		t.Fatalf("expected status code to be 200, but got: %d", w.Code)
+	}
+
+	data := struct {
+		Posts []*post
+	}{Posts: []*post{
+		{ID: 1, Title: "post one title"},
+		{ID: 2, Title: "post two title"},
+	}}
+	app.assertJSON(w.Body.Bytes(), data, t)
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 
 }
